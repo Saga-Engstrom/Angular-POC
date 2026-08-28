bae7840221399cbe8525aa52ceeb1c2c52de0f7b
Saga Engström <Saga97sle@hotmail.com>
2026-08-28 13:36:19 +0200

    final

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,9 @@ import { Component, signal } from '@angular/core';
 import { Todos } from './todos/todos';
 
 @Component({
-  imports: [Todos]
+  imports: [Todos],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('angular');
-}
+export class App {}
--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../models/todo';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Todo } from '../models/Todo';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todos.html',
   styleUrl: './todos.css',
 })
 export class Todos implements OnInit {
   todos!: Todo[];
+
+  inputTodo: string = '';
+
   constructor() {}
 
   ngOnInit(): void {
@@ -16,5 +21,23 @@ export class Todos implements OnInit {
       { content: 'First ToDo', completed: false },
       { content: 'Second ToDo', completed: true },
     ];
+  }
+
+  toggleDone(id: number) {
+    this.todos.forEach((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v;
+    });
+  }
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+
+    this.inputTodo = '';
   }
 }
